92cea997e3703654c4cf70018eb7eb4c168d27b9
backsooman <qortn311@naver.com>
2015-01-09 22:30:21 +0900

    Update eclipse.java

--- eclipse.java
+++ eclipse.java
@@ -1,7 +1,9 @@
 package file;
 
+import java.awt.FlowLayout;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -10,10 +12,13 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 class Test {
-	public static void playSound()
+	public static void playSound(String path)
 	{
-		File file=new File("C:\\Users\\수만\\workspace\\sooman\\src\\file\\Kelly Clarkson - Because Of You.wav");
+		File file=new File(path);
 		AudioInputStream audioInputStream=null;
 		SourceDataLine auline=null;
 		try{
@@ -62,10 +67,31 @@ class Test {
 			auline.close();
 		}
 	}
+	public static Action music(){
+			playSound("C:\\Users\\수만\\workspace\\sooman\\src\\file\\Adele - Rolling In The Deep.wav");
+		return null;
+	}
 	public static void main(String[]args){
-		playSound();
-		System.out.println("\n");
-		System.out.println("\\n");
-		
+		System.out.printf("1.Because of you-Kelly clarkson\n2.Rolling in the deep-Adele\n3.Safe&Sound-Taylor Swift\n4.Counting Stars-One Republic\n5.Party in the U.S.A-Miley Cyrus\n선택 : ");
+		Scanner keyboard=new Scanner(System.in);
+		int num=keyboard.nextInt();
+		switch(num){
+		case 1 : 
+			playSound("C:\\Users\\수만\\workspace\\sooman\\src\\file\\Kelly Clarkson - Because Of You.wav");
+			break;
+		case 2 :
+			playSound("C:\\Users\\수만\\workspace\\sooman\\src\\file\\Adele - Rolling In The Deep.wav");
+			break;
+		case 3 :
+			playSound("C:\\Users\\수만\\workspace\\sooman\\src\\file\\Taylor Swift ft. The Civil Wars - Safe &amp; Sound (The Hunger Games).wav");
+			break;
+		case 4 :
+			playSound("C:\\Users\\수만\\workspace\\sooman\\src\\file\\One Republic - Counting Stars - Lyrics Video (Native Album) [HD][HQ].wav");
+			break;
+		case 5 :
+			playSound("C:\\Users\\수만\\workspace\\sooman\\src\\file\\Miley Cyrus - Party In The U.S.A. - Official Music Video (HD) (1).wav");
+			break;
+		}
 	}
 }
+
